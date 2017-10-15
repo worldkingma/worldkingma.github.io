2f2c5a871ce82a9f42d314d17beccb713c139036
worldma <worldkingma@gmail.com>
2017-10-15 20:19:07 +0800

    i love china,so add the click show text

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 $(document).ready(function() {
+  var $body = $("body"),
+      a_idx = 0;
+  $body.click(function (e) {
+    var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+    var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+    var x = e.pageX,
+        y = e.pageY;
+    $i.css({
+      "z-index": 999999999999999999999999999999999999999999999999999999999999999999999,
+      "top": y - 20,
+      "left": x,
+      "position": "absolute",
+      "font-weight": "bold",
+      "color": "#24c789"
+    });
+    $body.append($i);
+    $i.animate({
+      "top": y - 180,
+      "opacity": 0
+    }, 1500, function () {
+      $i.remove();
+    });
+  });
 
   $('a.blog-button').click(function() {
     // If already in blog, return early without animate overlay panel again.
